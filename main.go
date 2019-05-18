66090e2a270daef570836cf6653ba8647a7dadd2
Araquach <adam@contra-design.com>
2019-05-18 15:03:30 +0100

    First Push

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"static-site/views"
+)
+
+var homeView *views.View
+var contactView *views.View
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(homeView.Render(w, nil))
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(contactView.Render(w, nil))
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
+}
